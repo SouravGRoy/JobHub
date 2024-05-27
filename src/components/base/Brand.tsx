49ca98046ab3b8ff079d57007ad8e160b3aa7f3e
sourav guha roy <souravroy94375@gmail.com>
2024-05-27 16:38:38 +0530

    Brand component: Display company branding.

---
+++ src/components/base/Brand.tsx
@@ -0,0 +1,12 @@
+const Brand = () => {
+  return (
+    <a
+      className="hover:bg-green-100  border border-green-100 rounded-lg cursor-pointer  md:flex items-center mx-1"
+      href="/"
+    >
+      <img className="w-12 " src="images/logo.png" alt="logo" />
+    </a>
+  );
+};
+
+export default Brand;
