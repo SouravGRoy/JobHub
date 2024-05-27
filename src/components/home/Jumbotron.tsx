ca5802a87237a4960761d6627172ab1bebd35a91
sourav guha roy <souravroy94375@gmail.com>
2024-05-27 18:26:28 +0530

    Add Jumbotron component: Highlight key information on the home page.

---
+++ src/components/home/Jumbotron.tsx
@@ -0,0 +1,23 @@
+const Jumbotron = () => {
+  return (
+    <section className="container dark:bg-gray-900">
+      <div className="pt-8 mt-6 mx-auto max-w-screen-xl text-center lg:text-start lg:py-10">
+        <h1 className="text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-white">
+          The Biggest Online
+        </h1>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-white">
+          Coding Job Board ✨
+        </h1>
+        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+          We connect developers to the best online coding jobs in the world,
+          only found on JobHub.com
+        </p>
+        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+          only found on JobHub.com
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Jumbotron;
