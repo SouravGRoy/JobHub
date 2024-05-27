7d494cf2c4eb4e253f108601911651c7e54e2410
sourav guha roy <souravroy94375@gmail.com>
2024-05-28 00:35:15 +0530

    Add home page component: Main landing page layout containing jumbotron,testamonial,marquee

---
+++ src/components/home/home.tsx
@@ -0,0 +1,15 @@
+import Jumbotron from "./Jumbotron";
+import MarqueeComponent from "./Marque";
+import Testamonial from "./testamonial";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Jumbotron />
+      <Testamonial />
+      <MarqueeComponent />
+    </div>
+  );
+};
+
+export default Home;
