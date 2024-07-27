8e4d72f43dc5b5ee1adc802da591b339bc4a20c5
sourav guha roy <souravroy94375@gmail.com>
2024-07-27 16:09:16 +0530

    Modified filter

--- src/components/filter/FilterFetch.tsx
+++ src/components/filter/FilterFetch.tsx
@@ -3,23 +3,19 @@ import React from "react";
 
 interface JobFetchProps {
   data: JobData[];
-  selectedRoles: string[]; // Add selectedRoles prop
-  setSelectedRoles: React.Dispatch<React.SetStateAction<string[]>>; // Add setSelectedRoles prop
+  selectedRoles: string[];
+  setSelectedRoles: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
 const JobFetch = ({ data, selectedRoles, setSelectedRoles }: JobFetchProps) => {
-  // Change selectedRoles to selectedRole
   const uniqueRoles = Array.from(new Set(data.map((item) => item.jobRole)));
 
-  // Handle change event on checkboxes
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const jobRole = event.target.name;
     setSelectedRoles((prevSelected) => {
       if (prevSelected.includes(jobRole)) {
-        // If role is already selected, remove it
         return prevSelected.filter((item) => item !== jobRole);
       } else {
-        // If role is not selected, add it
         return [...prevSelected, jobRole];
       }
     });
@@ -27,7 +23,7 @@ const JobFetch = ({ data, selectedRoles, setSelectedRoles }: JobFetchProps) => {
 
   return (
     <div>
-      <h3 className="mb-2 font-semibold text-white dark:text-white">Role</h3>
+      <h3 className="mb-2 font-semibold text-white">Role</h3>
       <ul className="w-48 text-sm font-medium text-white rounded-lg dark:bg-gray-700 dark:border-gray-600">
         {uniqueRoles.map((jobRole, index) => (
           <li className="w-full rounded-t-lg dark:border-gray-600" key={index}>
@@ -50,8 +46,7 @@ const JobFetch = ({ data, selectedRoles, setSelectedRoles }: JobFetchProps) => {
         ))}
       </ul>
       <div className="mt-4 text-white">
-        Selected Role: {selectedRoles.join(", ")}{" "}
-        {/* Change to "Selected Role" */}
+        Selected Role: {selectedRoles.join(", ")}
       </div>
     </div>
   );
